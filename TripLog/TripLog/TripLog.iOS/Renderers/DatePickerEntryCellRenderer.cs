913cecd0dd8b6ababd4d2a8536ad428a280543ba
Yavor Yankov <yavakaa@gmail.com>
2019-06-25 20:05:05 +0100

    Custom Renderer - DatePickerEntryCell iOS
    - Created a TableView DatePicker
    - Add DatePickerEntryCellRenderer in iOS project

---
+++ TripLog/TripLog/TripLog.iOS/Renderers/DatePickerEntryCellRenderer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using TripLog.Controls;
+using TripLog.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePickerEntryCell), typeof(DatePickerEntryCellRenderer))]
+namespace TripLog.iOS.Renderers
+{
+    public class DatePickerEntryCellRenderer : EntryCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            var datePickerCell = (DatePickerEntryCell)item;
+            UITextField textField = null;
+
+            if (cell != null)
+            {
+                textField = (UITextField)cell.ContentView.Subviews[0];
+            }
+
+            //Default datepicker display attributes
+            var mode = UIDatePickerMode.Date;
+            var displayFormat = "d";
+            var date = NSDate.Now;
+            var isLocalTime = false;
+
+            //Update datepicker based on Cell`s properties
+            if (datePickerCell != null)
+            {
+                if (datePickerCell.Date.Kind == DateTimeKind.Unspecified)
+                {
+                    var local = new DateTime(datePickerCell.Date.Ticks,
+                        DateTimeKind.Local);
+                    date = (NSDate)local;
+                }
+                else
+                {
+                    date = (NSDate)datePickerCell.Date;
+                }
+
+                isLocalTime = datePickerCell.Date.Kind == DateTimeKind.Local
+                    || datePickerCell.Date.Kind == DateTimeKind.Unspecified;
+            }
+
+            //Create iOS datepicker
+            var datePicker = new UIDatePicker
+            {
+                Mode = mode,
+                BackgroundColor = UIColor.White,
+                Date = date,
+                TimeZone = isLocalTime ? NSTimeZone.LocalTimeZone : new NSTimeZone("UTC")
+          
+            };
+
+            //Create a toolbar with a done button that will close the datePicker and set the selected value
+            var done = new UIBarButtonItem("Done", UIBarButtonItemStyle, (s, e) =>
+            {
+                var pickedDate = (DateTime)datePicker.Date;
+
+                if (isLocalTime)
+                {
+                    pickedDate = pickedDate.ToLocalTime();
+                }
+
+                //Update the value of the UITextField within the Cell
+                if (textField!=null)
+                {
+                    textField.Text = pickedDate.ToString(displayFormat);
+                    textField.ResignFirstResponder();
+                }
+
+                //Update the Date property on the Cell
+                if (datePickerCell!=null)
+                {
+                    datePickerCell.Date = pickedDate;
+                    datePickerCell.SendCompleted();
+                }
+            });
+
+            var toolbar = new UIToolbar
+            {
+                BarStyle = UIBarStyle.Default,
+                Translucent = false
+            };
+
+            toolbar.SizeToFit();
+            toolbar.SetItems(new[] { done }, true);
+
+            //Set the input view, toolbar and initial value for the Cell`s UITextField
+            if (textField != null)
+            {
+                textField.InputView = datePicker;
+                textField.InputAccessoryView = toolbar;
+
+                if (datePickerCell != null)
+                {
+                    textField.Text = datePickerCell.Date.ToString(displayFormat);
+                }
+            }
+
+            return cell;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ TripLog/TripLog/TripLog/Controls/DatePickerEntryCell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TripLog.Controls
+{
+    //Custom renderer for DatePicker
+    public class DatePickerEntryCell : EntryCell
+    {
+        public static readonly BindableProperty DateProperty =
+            BindableProperty.Create(
+                nameof(Date),
+                typeof(DateTime),
+                typeof(DatePickerEntryCell),
+                DateTime.Now,
+                BindingMode.TwoWay);
+
+        public DateTime Date
+        {
+            get
+            {
+                return (DateTime)GetValue(DateProperty);
+            }
+            set
+            {
+                SetValue(DateProperty, value);
+            }
+        }
+
+        public new event EventHandler Completed;
+
+        public new void SendCompleted()
+        {
+            Completed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
